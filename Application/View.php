661011d853e491f2b4b8929d79ea6a37685468e9
bermud <bermud@memphis.server>
2017-01-30 23:16:33 +0300

    29/01/2017 21:10 update 1.162

--- Application/View.php
+++ Application/View.php
@@ -21,9 +21,18 @@ class View extends \Application\PHPRoll
     public function script(array $params =[]) {
         if (!isset($params['id']) && !isset($params['name'])) throw new \Exception('Application\View::script() - необходимо определить id или name!');
         $id = $params['id'] ?? $params['name'];
-        $charset = $params['charset'] ?? 'UTF-8';
+        if (empty($id)) trigger_error("Application\View::script(...) пустой идентификатор скрипта", E_USER_WARNING);
+        $charset = isset($params['charset']) ? 'charset="'.$params['charset'].'"' : '';
+        $arguments = isset($params['arguments']) ? 'arguments="'.$params['arguments'].'"' : '';
+        $before = isset($params['before']) ? 'before="'.$params['before'].'"' : '';
+        $after = isset($params['after']) ? 'after="'.$params['after'].'"' : '';
         $opt = $params['opt'] ?? [];
-        $content = "<script id=\"$id\" type=\"text/x-template\" charset=\"$charset\">";
+        $vars = '';
+        $a = array_intersect_key( $params, array_flip( preg_grep( '/^tmpl\-.*/i', array_keys( $params ))));
+        if (count($a)) array_walk($a, function(&$item, $key) use(&$vars) { $vars = $key.'="'.$item.'" '; });
+$content =<<<EOT
+<script id="$id" type="text/x-template" $charset $arguments $vars $before $after>
+EOT;
         if ($params['src']) $content .= $this->context($params['src'], array_merge($opt,['script'=>null,'ext'=>'']));
         else if ($params['code'] || $params['body']) $content .= $params['code'] ?? $params['body'];
         else trigger_error("Application\View::script([name='$id']) пустой скрипт", E_USER_WARNING);
